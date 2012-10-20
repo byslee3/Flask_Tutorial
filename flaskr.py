05825aa4feedfb7b69d397b40f6ba7c92eb282c2
Christian <moonlimb@gmail.com>
2012-10-19 17:26:49 -0700

    Adding the html files and finishign the .py file

--- flaskr.py
+++ flaskr.py
@@ -39,8 +39,52 @@ def teardown_request(exception):
 #The functions below allow the users to interact with the site
 @app.route('/')
 def show_entries():
-    cursor = g.db.
+    cursor = g.db.execute('SELECT title, text FROM entries ORDER BY id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cursor.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('INSERT INTO entries (title, text) VALUES (?, ?)',
+                [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = "Invalid username"
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = "Invalid password"
+        else:
+            session['logged_in'] = True
+            flash("You were logged in")
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
 
 #This makes the server run
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
+
+
+
+
+
+
